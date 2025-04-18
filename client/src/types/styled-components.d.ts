ca5ea6e6f87c57a675e415c8dac04e3afd57e248
unknown <chengxi724@gmail.com>
2025-04-18 13:09:31 +0200

    fix: update styled-components type definitions

--- client/src/types/styled-components.d.ts
+++ client/src/types/styled-components.d.ts
@@ -1,3 +1,5 @@
+import 'styled-components';
+
 declare module 'styled-components' {
   export interface DefaultTheme {
     // Add your theme properties here
@@ -8,4 +10,20 @@ declare module 'styled-components' {
       text: string;
     };
   }
+
+  export interface StyledComponentBase<
+    C extends string | React.ComponentType<any>,
+    T extends object,
+    O extends object = {},
+    A extends keyof any = never
+  > extends React.FC<O & A> {
+    withComponent<C2 extends string | React.ComponentType<any>>(
+      component: C2
+    ): StyledComponentBase<C2, T, O, A>;
+  }
+
+  export function styled<T extends object = DefaultTheme>(
+    tag: TemplateStringsArray | string | React.ComponentType<any>,
+    ...styles: any[]
+  ): StyledComponentBase<any, T>;
 } 
