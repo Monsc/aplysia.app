27d135fa601c659c6be30ccddcc22751581a050b
unknown <chengxi724@gmail.com>
2025-04-18 13:04:37 +0200

    feat: add styled-components type definitions and update dependencies

---
+++ client/src/types/styled-components.d.ts
@@ -0,0 +1,11 @@
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    // Add your theme properties here
+    colors: {
+      primary: string;
+      secondary: string;
+      background: string;
+      text: string;
+    };
+  }
+} 
