10bd4f974ee5e60892e4a1b04f35dafb2e1b32f1
unknown <chengxi724@gmail.com>
2025-04-18 16:47:25 +0200

    fix: update Stripe secret key environment variable name

--- server/schemas/mutations/paymentMutation.js
+++ server/schemas/mutations/paymentMutation.js
@@ -6,7 +6,7 @@ const Stripe = require('stripe');
 const dotenv = require('dotenv')
 
 dotenv.config()
-const stripe = new Stripe(process.env.STRIPE_SK);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const {
     GraphQLID,
