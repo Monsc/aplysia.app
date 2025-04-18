a10ee18fd9177041d9f2d5b71f1cd2ce5a4e491d
unknown <chengxi724@gmail.com>
2025-04-18 15:02:16 +0200

    Update Apollo Client configuration and Vercel settings

--- client/src/apollo/client.ts
+++ client/src/apollo/client.ts
@@ -1,12 +1,28 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
   uri: process.env.REACT_APP_API_URL || 'http://localhost:5002/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const user = JSON.parse(localStorage.getItem('user') || '{}');
+  return {
+    headers: {
+      ...headers,
+      authorization: user.accessToken ? `Bearer ${user.accessToken}` : '',
+    }
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-and-network',
+    },
+  },
 });
 
 export default client;
